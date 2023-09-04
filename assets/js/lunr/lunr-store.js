807f7d13bf8353f9ded9f670fd87e8a6fecb27b3
romain-jacob <romain-jacob@users.noreply.github.com>
2023-09-04 20:24:41 +0000

    jekyll build from Action d55f5625e4946dcec1ed750bacfb509748a4ff3f

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Official creation",
+        "excerpt":"It’s our great pleasure to announce the creation of ReScience which is a peer-reviewed journal that targets computational research and encourages the explicit replication of already published research, promoting new and open-source implementations in order to ensure that the original research is reproducible.  ","categories": ["news"],
+        "tags": [],
+        "url": "/news/2015/09/01/journal-start.html",
+        "teaser": null
+      }]
